1b4d93d4fb145550f0c67b8cc5a5b43eb1ac8399
Christian Schulze <christian.schulze@mni.thm.de>
2016-03-03 15:26:44 +0100

    [get_id_for_mail] fixed mem leaks; improved code

--- src/libidroplink.c
+++ src/libidroplink.c
@@ -317,62 +317,79 @@ int deauthenticate(char *api_endpoint, char *id, char *token, struct error *err)
 char *get_id_for_email(char *api_endpoint, char *email, char *password, struct error *err)
 {
     CURL *curl;
-    CURLcode res;
-    char *url,
-         *opts,
-         *out;
+    char *out;
+    long http_code = 0;
 
     out = NULL;
     curl = curl_easy_init();
 
     if (curl != NULL) {
-        char *user_agent;
+        char *post_fields,
+             *url,
+             *user_agent_header;
+        CURLcode res;
         struct curl_string s;
-        struct curl_slist *chunk = NULL;
+        struct curl_slist *header_chunk = NULL;
+
         init_curl_string(&s);
 
-        user_agent = malloc(sizeof(char *) * (26 + 1));
-        if (user_agent == NULL) {
+        /* User agent header */
+        user_agent_header = malloc(sizeof(char *) * (26 + 1));
+        if (user_agent_header == NULL) {
             fprintf(stderr, "malloc() failed\n");
             exit(EXIT_FAILURE);
         }
+        sprintf(user_agent_header, "User-Agent: libidroplink/%d", IDL_VERSION);
 
-        sprintf(user_agent, "User-Agent: libidroplink/%d", IDL_VERSION);
-        chunk = curl_slist_append(chunk, user_agent);
+        header_chunk = curl_slist_append(header_chunk, user_agent_header);
 
-        opts = malloc((strlen(email) + strlen(password) + 16 + 1) * sizeof(char));
-        if (opts == NULL) {
+        /* POST fields */
+        post_fields = malloc((strlen(email) + strlen(password) + 16 + 1) * sizeof(char));
+        if (post_fields == NULL) {
             fprintf(stderr, "malloc() failed\n");
             exit(EXIT_FAILURE);
         }
-
-        sprintf(opts, "email=%s&password=%s", email, password);
+        sprintf(post_fields, "email=%s&password=%s", email, password);
 
         url = join_url(api_endpoint, "/users", email, "/idformail", NULL);
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_POST, url);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, opts);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_chunk);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _write_curl_result_string);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
 
         res = curl_easy_perform(curl);
+        curl_slist_free_all(header_chunk);
+        free(post_fields);
+        free(url);
+        free(user_agent_header);
 
         if (s.p != NULL) {
             cJSON *root = cJSON_Parse(s.p);
 
-            out = strdup(cJSON_GetObjectItem(root, "_id")->valuestring);
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+            if (http_code == 200 && cJSON_GetObjectItem(root, "_id") != NULL) {
+                out = strdup(cJSON_GetObjectItem(root, "_id")->valuestring);
+            } else {
+                if (cJSON_GetObjectItem(root, "message") != NULL) {
+                    err->description = strdup(cJSON_GetObjectItem(root, "message")->valuestring);
+                    err->version = error_version;
+                    err->http_code = http_code;
+                }
+            }
+
+            free(s.p);
             cJSON_Delete(root);
         }
 
-        free(s.p);
-        free(opts);
+        curl_easy_cleanup(curl);
     } else {
         err->description = "Unable to prepare CURL";
         err->version = error_version;
+        err->http_code = http_code;
     }
-
-    curl_easy_cleanup(curl);
 
     return out;
 }
