1ef7c4d779dd80b100989df6ce4b5a3024b7c65d
Christian Schulze <christian.schulze@mni.thm.de>
2016-03-03 15:27:47 +0100

    [get_user] fixed mem leaks; improved code

--- src/libidroplink.c
+++ src/libidroplink.c
@@ -397,9 +397,7 @@ char *get_id_for_email(char *api_endpoint, char *email, char *password, struct e
 struct user *get_user(char *api_endpoint, char *id, char *token, struct error *err)
 {
     CURL *curl;
-    CURLcode res;
-    char *url,
-         *opts;
+    long http_code = 0;
     struct user *usr = malloc(sizeof(struct user));
 
     if (usr == NULL) {
@@ -410,84 +408,94 @@ struct user *get_user(char *api_endpoint, char *id, char *token, struct error *e
     curl = curl_easy_init();
 
     if (curl != NULL) {
-        char *user_agent;
+        char *auth_header,
+             *url,
+             *user_agent_header;
+        CURLcode res;
         struct curl_string s;
-        struct curl_slist *chunk = NULL;
+        struct curl_slist *header_chunk = NULL;
 
         init_curl_string(&s);
 
-        /* Build custom header list for `Authorization` and `UserAgent`. */
-        opts = malloc((strlen(token) + 16 + 1) * sizeof(char));
-        if (opts == NULL) {
+        /* Authorization header */
+        auth_header = malloc((strlen(token) + 16 + 1) * sizeof(char));
+        if (auth_header == NULL) {
             fprintf(stderr, "malloc() failed\n");
             exit(EXIT_FAILURE);
         }
+        sprintf(auth_header, "Authorization: %s", token);
 
-        sprintf(opts, "Authorization: %s", token);
-        chunk = curl_slist_append(chunk, opts);
+        header_chunk = curl_slist_append(header_chunk, auth_header);
 
-        user_agent = malloc(sizeof(char *) * (26 + 1));
-        if (user_agent == NULL) {
+        /* User agent header */
+        user_agent_header = malloc(sizeof(char *) * (26 + 1));
+        if (user_agent_header == NULL) {
             fprintf(stderr, "malloc() failed\n");
             exit(EXIT_FAILURE);
         }
+        sprintf(user_agent_header, "User-Agent: libidroplink/%d", IDL_VERSION);
 
-        sprintf(user_agent, "User-Agent: libidroplink/%d", IDL_VERSION);
-        chunk = curl_slist_append(chunk, user_agent);
+        header_chunk = curl_slist_append(header_chunk, user_agent_header);
 
         url = join_url(api_endpoint, "/users", id, NULL);
         curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_chunk);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _write_curl_result_string);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
 
         res = curl_easy_perform(curl);
-        curl_slist_free_all(chunk);
+        curl_slist_free_all(header_chunk);
+        free(auth_header);
+        free(url);
+        free(user_agent_header);
 
         if (s.p != NULL) {
             cJSON *root = cJSON_Parse(s.p);
 
-            if (cJSON_GetObjectItem(root, "_id") != NULL) {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+            if (http_code == 200 && cJSON_GetObjectItem(root, "_id") != NULL) {
                 usr->_id = strdup(cJSON_GetObjectItem(root, "_id")->valuestring);
             } else {
                 usr->_id = NULL;
             }
 
-            if (cJSON_GetObjectItem(root, "email") != NULL) {
+            if (http_code == 200 && cJSON_GetObjectItem(root, "email") != NULL) {
                 usr->email = strdup(cJSON_GetObjectItem(root, "email")->valuestring);
             } else {
                 usr->email = NULL;
             }
 
-            if (cJSON_GetObjectItem(root, "creation_date") != NULL) {
+            if (http_code == 200 && cJSON_GetObjectItem(root, "creation_date") != NULL) {
                 usr->creation_date = strdup(cJSON_GetObjectItem(root, "creation_date")->valuestring);
             } else {
                 usr->creation_date = NULL;
             }
 
-            if (usr->_id == NULL && usr->email == NULL && usr->creation_date == NULL) {
+            if (http_code == 200 && usr->_id == NULL && usr->email == NULL && usr->creation_date == NULL) {
                 if (cJSON_GetObjectItem(root, "message") != NULL) {
                     err->description = strdup(cJSON_GetObjectItem(root, "message")->valuestring);
                     err->version = error_version;
+                    err->http_code = http_code;
                 } else {
                     err->description = "Unexpected answer from remote";
                     err->version = error_version;
+                    err->http_code = http_code;
                 }
 
                 usr = NULL;
             }
 
             cJSON_Delete(root);
+            free(s.p);
         }
 
-        free(s.p);
-        free(opts);
+        curl_easy_cleanup(curl);
     } else {
         err->description = "Unable to prepare CURL";
         err->version = error_version;
+        err->http_code = http_code;
     }
-
-    curl_easy_cleanup(curl);
 
     return usr;
 }
